1a73041005a4ccda1bec54e0481758c6a8e3fb12
David J Birnbaum <djbpitt@gmail.com>
2014-12-14 13:25:04 -0500

    First steps toward toggling line numbering

--- numberlines.js
+++ numberlines.js
@@ -88,6 +88,7 @@ function numberLines(block) {
     block.innerHTML = "";
     block.appendChild(rebuilt.cloneNode(true));
     block.style.height=(lines.length * 1.1) + 'em';
+    block.classList.contains('numbered') ? true : block.classList.add('numbered');
 }
 function getNodesAndOffsets(root, startPosition, endPosition) {
     var nodeIterator = document.createNodeIterator(root, NodeFilter.SHOW_TEXT);
